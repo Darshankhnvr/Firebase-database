e077e361c24f0ce190a258981f96484d0d730f4a
Darshan K <163309931+Darshankhnvr@users.noreply.github.com>
2025-02-09 23:12:32 +0530

    Update firebaseConfig.js

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -6,15 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB26PR4RAs-t5GBOxuIv38jz19lvxEU9D8",
-  authDomain: "test-94ceb.firebaseapp.com",
-  projectId: "test-94ceb",
-  storageBucket: "test-94ceb.firebasestorage.app",
-  messagingSenderId: "151865157202",
-  appId: "1:151865157202:web:41575f46a652fa83cfa18b",
-  measurementId: "G-VCXLHP6YZ8"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
